c20155baea49a76e2e75de65c85d7cad647a7558
ankit-gupta12 <ankit.kumar.bharwari@gmail.com>
2018-12-04 08:00:08 +0530

    [04 Dec Ankit ] Initial CheckIn

---
+++ MvcAffableBean/Models/CustomerDetail.cs
@@ -0,0 +1,55 @@
+﻿using MvcAffableBean.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcAffableBean.Models
+{
+    public class CustomerDetail
+    {
+        MvcAffableBeanContext db = new MvcAffableBeanContext();
+        public void SaveCustomerDetail(Customer customerdetail)
+        {
+            if(customerdetail !=null)
+            {
+                //if user exists , update other details.
+                var existinguser = db.Customers.Where(x => x.UserId == customerdetail.UserId).FirstOrDefault();
+                if(existinguser !=null)
+                {
+                    existinguser.Address = customerdetail.Address;
+                    existinguser.FirstName = customerdetail.FirstName;
+                    existinguser.LastName = customerdetail.LastName;
+                    //db.Customers.Add(existinguser);
+                    db.Entry(existinguser).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                }                
+                //customerdetail = new Customer
+                //{
+                //    FirstName = customerdetail.FirstName,
+                //    LastName = customerdetail.LastName,                    
+                //    UserId = customerdetail.UserId
+                //};
+               // db.Customers.Add(customerdetail);
+                //db.SaveChanges();
+            }
+        }
+        public int SaveMobileNumber(Customer customerdetail)
+        {
+            int Id=0;
+            if(customerdetail !=null)
+            {
+                var existinguser = db.Customers.Where(x => x.UserId == customerdetail.UserId).FirstOrDefault();
+                if (existinguser == null)
+                {
+                    db.Customers.Add(customerdetail);
+                    db.SaveChanges();
+                    Id = customerdetail.Id;
+                }
+            }
+            return Id;
+        }
+    }
+}
---
+++ MvcAffableBean/Models/OrderDetail.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcAffableBean.Models
+{
+    public class OrderDetail :Common
+    {
+        [Key]
+        public int Id { get; set; }
+        // [Required]
+        
+        [Display(Name = "Order Id")]
+        public int OrderId { get; set; }
+
+        // [Required]
+        [Display(Name = "User Id"), MaxLength(50)]
+        public string UserId { get; set; }
+
+        //[Required]
+        [Display(Name = "Address"), MaxLength(500), DisplayFormat(ConvertEmptyStringToNull = false)]
+        public string Address { get; set; }
+        [Display(Name = "Cooking Instruction"), MaxLength(500), DisplayFormat(ConvertEmptyStringToNull = true)]
+        public string CookingInstruction { get; set; }
+
+        [Display(Name = "Delivery Status"),  DisplayFormat(ConvertEmptyStringToNull = false)]
+        public int ? DeliveryStatus { get; set; }
+
+        [Display(Name = "Delivery Note"), MaxLength(500), DisplayFormat(ConvertEmptyStringToNull = false)]
+        public string DeliveryNote { get; set; }
+
+        [Display(Name = "Amount")]
+        public decimal ? Amount { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
